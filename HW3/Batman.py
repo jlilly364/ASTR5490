5993cae6973bd4b17c8ead81d26154c435dc71af
Jimmy Lilly <jimmy@tau.uwyo.edu>
2020-11-03 18:29:59 -0700

    Finished matching batman model to phase-folded data for #6

--- HW3/Batman.py
+++ HW3/Batman.py
@@ -9,9 +9,7 @@ import batman
 import numpy as np
 import matplotlib.pyplot as plt
 
-def BatmanModel(t0=0.0,P=1.0,rad_pl=0.1,a=15.0,i=87.0,e=0.0,w=90.0,coeff=[0.5,0.1]):
-    
-    fig,ax = plt.subplots()
+def BatmanModel(t0=0.0,P=1.0,rad_pl=0.1,a=15.0,i=87.0,e=0.0,w=90.0,coeff=[0.5,0.1],plot=False):
     
     # Initialize the model
     params = batman.TransitParams()       #object to store transit parameters
@@ -25,16 +23,16 @@ def BatmanModel(t0=0.0,P=1.0,rad_pl=0.1,a=15.0,i=87.0,e=0.0,w=90.0,coeff=[0.5,0.
     params.limb_dark = "quadratic"        #limb darkening model
     params.u = coeff#[0.4899, 0.1809]     #limb darkening coefficients [u1, u2]
     
-    t = np.linspace(-.05, .05, 1000)  #times at which to calculate light curve
+    t = np.linspace(-.25, .25, 1000)  #times at which to calculate light curve
     t_new = np.interp(t, (t.min(), t.max()), (0, 0.2))
     m = batman.TransitModel(params, t)    #initializes model
     
     flux = m.light_curve(params)                    #calculates light curve
     
-    ax.get_yaxis().get_major_formatter().set_useOffset(False)
-    ax.plot(t_new,flux)
-    #plt.ylim(np.min(flux),np.max(flux))
-    #plt.hist(flux)
+    if plot==True:
+        fig,ax = plt.subplots()
+        ax.get_yaxis().get_major_formatter().set_useOffset(False)
+        ax.plot(t_new,flux)
 
     return (t_new,flux)
 #BatmanModel()
