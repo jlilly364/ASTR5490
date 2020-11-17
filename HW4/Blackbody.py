7289ffc01d694a4c042e32a3d4e4ff920b6a209c
Jimmy Lilly <jimmy@tau.uwyo.edu>
2020-11-16 18:56:49 -0700

    Added functionality to SED class to plot three different y-axes vs. wavelength or frequency. Completed 1c of HW4

--- HW4/Blackbody.py
+++ HW4/Blackbody.py
@@ -15,10 +15,10 @@ import time
 # Class to generate and analze spectral energy distributions (SEDs)
 class SED:
     
-    def __init__(self,xvariable,Teff=5780,lambda_min=10**-10,lambda_max=10**-4,N=2*10**5):
+    def __init__(self,xvariable,yvariable,Teff=5780,lambda_min=10**-10,lambda_max=10**-4,N=10**5):
         # Inputs:
         #   xvariable: 'freq' or 'wavelen' to determine which Planck form to use
-        #   yvariable: 'Planck' or 'integrated'
+        #   yvariable: 'planck' or 'luminosity' or 'xvar_lumin'
         #   T_eff: effective temperature of blackbody (in K)
         #   lambda_min: minimum wavelength to calc. Planck function over (in m)
         #   lambda_max: minimum wavelength to calc. Planck function over (in m)
@@ -26,6 +26,7 @@ class SED:
     
         # Cast initial parameters as global variables
         self.xvariable = xvariable
+        self.yvariable = yvariable
         self.Teff = Teff*u.K
         self.lambda_min = lambda_min*u.m
         self.lambda_max = lambda_max*u.m
@@ -46,8 +47,15 @@ class SED:
             self.xlabel = r'$\lambda$ ({0:latex_inline})'.format(self.x_min.unit)
             
             # Define v-line labels
-            self.vlabel = r'Analytical: $\lambda_{{max}}={0:.2e}$ {1:latex_inline}'.format(self.y_peak.value,self.y_peak.unit)
+            self.vlabel = r'Analytical (Planck): $\lambda_{{max}}={0:.2e}$ {1:latex_inline}'.format(self.y_peak.value,self.y_peak.unit)
             self.num_vlabel = r'Numerical: $\lambda_{{max}}={0:.2e}$ {1:latex_inline}'
+            
+            if self.yvariable == 'planck':
+                self.ylabel = r'Spectral Radiance per $\Omega$ ({0:latex_inline})'
+            elif self.yvariable == 'luminosity':
+                self.ylabel = r'$L_{{\lambda}}$ ({0:latex_inline})'
+            elif self.yvariable == 'xvar_lumin':
+                self.ylabel = r'$\lambda L_{{\lambda}}$ ({0:latex_inline})'
         
         elif self.xvariable == 'freq':
             # Convert wavelength range to frequency range
@@ -61,8 +69,17 @@ class SED:
             self.xlabel = r'$\nu$ ({0:latex_inline})'.format(self.x_min.unit)
             
             # Define v-line labels
-            self.vlabel = r'Analytical: $\nu_{{max}}={0:.2e}$ {1:latex_inline}'.format(self.y_peak.value,self.y_peak.unit)
+            self.vlabel = r'Analytical (Planck): $\nu_{{max}}={0:.2e}$ {1:latex_inline}'.format(self.y_peak.value,self.y_peak.unit)
             self.num_vlabel = r'Numerical: $\nu_{{max}}={0:.2e}$ {1:latex_inline}'
+            
+            if self.yvariable == 'planck':
+                self.ylabel = r'Spectral Radiance per $\Omega$ ({0:latex_inline})'
+            elif self.yvariable == 'luminosity':
+                self.ylabel = r'$L_{{\nu}}$ ({0:latex_inline})'
+            elif self.yvariable == 'xvar_luminos':
+                self.ylabel = r'$\nu L_{{\nu}}$ ({0:latex_inline})'
+        else:
+            print("Valid entries are 'wavelen' or 'freq'")
         
         # Calculate surface area of sun for later use
         self.sun_SA = 4*np.pi*const.R_sun**2
@@ -88,9 +105,6 @@ class SED:
                 B = prefactor.value*x.value**(-5)/(np.exp(exp_factor.value/x.value)-1)
             else:
                 B = prefactor*x**(-5)/(np.exp(exp_factor/x)-1)
-            
-            # Leave or flip sign of luminosity when integrating later
-            self.multiplier = 1.0
         
         elif self.xvariable == 'freq':
             
@@ -105,9 +119,6 @@ class SED:
                 B = prefactor.value*x.value**3/(np.exp(exp_factor.value*x.value)-1)
             else:
                 B = prefactor*x**3/(np.exp(exp_factor*x)-1)
-                
-            # Leave or flip sign of luminosity when integrating later
-            self.multiplier = -1.0
             
         return(B)
     
@@ -123,14 +134,41 @@ class SED:
         start_time = time.time()
         
         # Define list of wavelengths
-        x = np.linspace(self.x_min,self.x_max,self.N)
-        
+        if self.xvariable == 'wavelen':
+            x = np.linspace(self.x_min,self.x_max,self.N)
+        elif self.xvariable == 'freq':
+            x = np.linspace(self.x_max,self.x_min,self.N)
+            
         # Calculate Planck function at each wavelength
         y = self.Planck(x,units=True)
-    
+        
+        if self.yvariable == 'luminosity':
+            # Convert Planck function to luminosity
+            y *= np.pi*self.sun_SA
+            y = np.multiply(x,y)
+        elif self.yvariable == 'xvar_luminos':
+            # Convert Planck function to luminosity*xvariable (planck * x**2)
+            y *= np.pi*self.sun_SA
+            y = np.multiply(np.square(x),y)
+            
         # Find where blackbody peaks from my calculations
         peak_loc = np.argmax(y)
         numerical_max = x[peak_loc]
+        
+        if self.yvariable == 'planck':
+            # Calculate stellar luminosity (np.trapz integrates Planck func.)
+            luminosity = np.pi*self.sun_SA*np.trapz(y,x)
+            print("Luminosity = {0:.3f} Lsun".format(luminosity.to(u.erg/u.s)/const.L_sun.to(u.erg/u.s)))
+            
+            # Calculate fraction of luminosity from below peak wavelength
+            lumin_before = np.pi*self.sun_SA*np.trapz(y[:peak_loc],x[:peak_loc])
+            frac_before = lumin_before/luminosity*100
+            print("{0:.2f}% of energy emitted below peak".format(frac_before))
+            
+            # Calculate fraction of luminosity from peak wavelength and beyond
+            lumin_after = np.pi*self.sun_SA*np.trapz(y[peak_loc:],x[peak_loc:])
+            frac_after = lumin_after/luminosity*100
+            print("{:.2f}% of energy emitted above peak \n".format(frac_after))
         
         # Decide whether to plot SED or not
         if plot == True:
@@ -143,7 +181,7 @@ class SED:
             plt.xscale('log')
             
             # Plot analytical and numerical wavelength peaks
-            plt.vlines(self.y_peak.value,min(y).value,max(y).value,colors='red',\
+            plt.vlines(self.y_peak.value,min(y).value,max(y).value,colors='green',\
                        linestyles='dashed',label=self.vlabel)
             plt.vlines(numerical_max.value,min(y).value,max(y).value,colors='red',\
                        linestyles='dashed',label=self.num_vlabel.format(numerical_max.value,numerical_max.unit))
@@ -151,29 +189,16 @@ class SED:
             
             # Axes labels and titles
             plt.xlabel(self.xlabel,fontsize=14)
-            plt.ylabel(r'Spectral Radiance per $\Omega$ ({0:latex_inline})'.format(y.unit),fontsize=14)
+            plt.ylabel(self.ylabel.format(y.unit),fontsize=14)
             plt.title(r'Spectral Energy Distribution for $T_{eff}=$%dK Star'%self.Teff.value,fontsize=18)
             plt.tight_layout()
-        
-        # Calculate stellar luminosity (np.trapz integrates Planck func.)
-        luminosity = np.pi*self.sun_SA*np.trapz(y,x)*self.multiplier
-        print("Luminosity = {0:.3f} Lsun".format(luminosity.to(u.erg/u.s)/const.L_sun.to(u.erg/u.s)))
-        
-        # Calculate fraction of luminosity from below peak wavelength
-        lumin_before = np.pi*self.sun_SA*np.trapz(y[:peak_loc],x[:peak_loc])*self.multiplier
-        frac_before = lumin_before/luminosity*100
-        print("{0:.2f}% of energy emitted below peak".format(frac_before))
-        
-        # Calculate fraction of luminosity from peak wavelength and beyond
-        lumin_after = np.pi*self.sun_SA*np.trapz(y[peak_loc:],x[peak_loc:])*self.multiplier
-        frac_after = lumin_after/luminosity*100
-        print("{:.2f}% of energy emitted above peak \n".format(frac_after))
         
         # Tell user how long function took to run
         print('Program took %.2f sec to run'%(time.time()-start_time))
         
-        return(luminosity)
+        return(x,y)
 
-"""test = SED('wavelen')
+# Code to test class and functions
+"""test = SED('freq','xvar_luminos')
 #test.Planck(10**-6*u.m,units=False)
 test.SEDStar(True)"""
